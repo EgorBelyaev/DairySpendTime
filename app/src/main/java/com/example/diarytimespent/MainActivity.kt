4fa0e5c9f3a231e3ea5fbe02d7ac767de3c29f5d
egor <egor@yandex,ru>
2022-05-10 11:36:08 +0300

    add HomeFragment

--- app/src/main/java/com/example/diarytimespent/MainActivity.kt
+++ app/src/main/java/com/example/diarytimespent/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.diarytimespent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_home)
+
+        replaceFlagment(HomeFragment.newInstance(),true)
+    }
+
+    fun replaceFlagment(fragment: Fragment,istransition:Boolean){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition
+        }
+
+        fragmentTransition.replace(R.id.textView,fragment).addToBackStack(fragment.javaClass.simpleName)
     }
 }
